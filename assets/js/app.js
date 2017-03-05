816788b18a98c4363a5ed37e3393ebf804922727
miriampc <miiriampc@gmail.com>
2017-03-04 23:42:33 -0500

    primer commit en rama, implementando funcion calcularImc

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
 function calcularImc(){
-  
+  var name=document.getElementById('name').value;
+  var peso=document.getElementById('peso').value;
+  var talla=document.getElementById('talla').value;
+  var imc=document.getElementById('imc');
+  var mostrar="<br><h2>Tus datos</h2><ul><li>Nombre: "
+              +name+"</li><li>Peso: "+peso+"</li><li>Talla: "+talla+"</li><li>IMC: "+(peso/talla).toFixed(2)+"</li></ul>";
+  imc.innerHTML= mostrar;
 }
