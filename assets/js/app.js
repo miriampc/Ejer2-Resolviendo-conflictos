95ea7a2a7a599530d5d07a2ae27d5ec30c35b965
RuthSalvador <ruth.abby.sz@gmail.com>
2017-03-05 00:40:12 -0500

    agregando funcion para el calculo de IMC

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,12 @@
-function calcularImc(){
-  
+var nombre = document.getElementById('name');
+var peso = document.getElementById('peso');
+var talla = document.getElementById('talla');
+var calculo = document.getElementById('submit');
+
+calculo.onclick = function (){
+  if ((peso.value.length == 0) || (talla.value.length == 0)){
+    alert("Por favor completar los campos de peso y/o talla");
+  } else {
+  alert ("Hola " + nombre.value + " tu IMC es de " + (peso.value / (talla.value * talla.value)).toFixed(2));
+  }
 }
